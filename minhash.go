ad0e4718830160aba487cfbdec2176b21feffb67
Nick S <turgon@gmail.com>
2017-04-09 02:58:00 +0000

    added 32bit

--- minhash.go
+++ minhash.go
@@ -5,8 +5,11 @@ import (
 	"hash/fnv"
 )
 
+type newFn func(int) hash.Hash
+
 type MinHash8 []uint8
 type MinHash16 []uint16
+type MinHash32 []uint32
 
 func New8(sz int) hash.Hash {
 	n := make(MinHash8, sz)
@@ -24,6 +27,14 @@ func New16(sz int) hash.Hash {
 	return &n
 }
 
+func New32(sz int) hash.Hash {
+	n := make(MinHash32, sz)
+	for i := 0; i < sz; i++ {
+		n[i] = 4294967295
+	}
+	return &n
+}
+
 func (mh MinHash8) Reset() {
 	for i := 0; i < len(mh); i++ {
 		mh[i] = 255
@@ -33,6 +44,12 @@ func (mh MinHash8) Reset() {
 func (mh MinHash16) Reset() {
 	for i := 0; i < len(mh); i++ {
 		mh[i] = 65535
+	}
+}
+
+func (mh MinHash32) Reset() {
+	for i := 0; i < len(mh); i++ {
+		mh[i] = 4294967295
 	}
 }
 
@@ -46,6 +63,13 @@ func (mh MinHash8) Sum(in []byte) []byte {
 func (mh MinHash16) Sum(in []byte) []byte {
 	for i := 0; i < len(mh); i++ {
 		in = append(in, byte(mh[i] >> 8), byte(mh[i]))
+	}
+	return in
+}
+
+func (mh MinHash32) Sum(in []byte) []byte {
+	for i := 0; i < len(mh); i++ {
+		in = append(in, byte(mh[i] >> 24), byte(mh[i] >> 16), byte(mh[i] >> 8), byte(mh[i]))
 	}
 	return in
 }
@@ -88,8 +112,28 @@ func (mh MinHash16) Write(data []byte) (int, error) {
 	return len(data), nil
 }
 
+func (mh MinHash32) Write(data []byte) (int, error) {
+	h := fnv.New64a()
+	h.Write(data)
+	s := h.Sum64()
+
+	v1 := uint32(s)
+	v2 := uint32(s >> 32)
+
+	for i := 0; i < len(mh); i++ {
+		x := v1 + uint32(i)*v2
+		if x < mh[i] {
+			mh[i] = x
+		}
+	}
+
+	return len(data), nil
+}
+
 func (mh MinHash8) BlockSize() int { return 1 }
 func (mh MinHash16) BlockSize() int { return 1 }
+func (mh MinHash32) BlockSize() int { return 1 }
 
 func (mh MinHash8) Size() int { return len(mh) }
 func (mh MinHash16) Size() int { return len(mh) }
+func (mh MinHash32) Size() int { return len(mh) }
--- minhash_test.go
+++ minhash_test.go
@@ -1,6 +1,7 @@
 package minhash
 
 import (
+	"hash"
 	"bytes"
 	"testing"
 )
@@ -71,6 +72,39 @@ func TestMinHash16(t *testing.T) {
 	}
 }
 
+func TestMinHash32(t *testing.T) {
+	var x, b []byte
+	var l int = 1
+
+	mh := New32(l)
+
+	b = make([]byte, 0)
+	x = mh.Sum(b)
+
+	if !bytes.Equal(x, []byte{255, 255, 255, 255}) {
+		t.Error("Sum returned unexpected results", x)
+	}
+
+	mh.Write([]byte("test"))
+
+	mh.Reset()
+
+	b = make([]byte, 0)
+	x = mh.Sum(b)
+
+	if !bytes.Equal(x, []byte{255, 255, 255, 255}) {
+		t.Error("Sum returned unexpected results", x)
+	}
+
+	if mh.BlockSize() != 1 {
+		t.Error("BlockSize returned unexpected result")
+	}
+
+	if mh.Size() != l {
+		t.Error("Size returned unexpected result")
+	}
+}
+
 func TestFuzz(t *testing.T) {
 	b := make([]byte, 0)
 	if Fuzz(b) != -1 {
@@ -84,17 +118,32 @@ func TestFuzz(t *testing.T) {
 	}
 }
 
-func BenchmarkMinHash8(b *testing.B) {
+var resulth hash.Hash
+
+func BenchmarkNewMinHash8(b *testing.B) { benchmarkNewMinHash(New8, b) }
+func BenchmarkNewMinHash16(b *testing.B) { benchmarkNewMinHash(New16, b) }
+func BenchmarkNewMinHash32(b *testing.B) { benchmarkNewMinHash(New32, b) }
+
+func benchmarkNewMinHash(mhf newFn, b *testing.B) {
+	var mh hash.Hash
 	for n := 0; n < b.N; n++ {
-		mh := New8(128)
-		mh.Write([]byte("testing"))
+		mh = mhf(128)
 	}
+	resulth = mh
 }
 
-func BenchmarkMinHash16(b *testing.B) {
+func BenchmarkMinHash8(b *testing.B) { benchmarkMinHash(New8(128), b) }
+func BenchmarkMinHash16(b *testing.B) { benchmarkMinHash(New16(128), b) }
+func BenchmarkMinHash32(b *testing.B) { benchmarkMinHash(New32(128), b) }
+
+var result int
+
+func benchmarkMinHash(mh hash.Hash, b *testing.B) {
+	var r int
 	for n := 0; n < b.N; n++ {
-		mh := New16(128)
-		mh.Write([]byte("testing"))
+		mh.Reset()
+		r, _ = mh.Write([]byte("testing"))
 	}
+	result = r
 }
 
